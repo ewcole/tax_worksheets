56819ab765e38b7bbe3bef080bd29ff2586d7baf
Ed Cole <edcole@mail.sunyjcc.edu>
2020-03-02 17:59:48 -0500

    Start editing raw text for 2019 Federal F1040

--- Federal/parse1040dat.groovy
+++ Federal/parse1040dat.groovy
@@ -7,15 +7,17 @@ def stateStack = []
 class State {
   Closure processLine
   String  stateCode
+  String toString() {stateCode}
 }
 
 def pushState(State s) {
   stateStack.push(s);
 }
+
 def pushState = {
   String s ->
-    assert states[s]
-    stateStack.push(states[s]);
+  assert states[s]
+  stateStack.push(states[s]);
 }
 
 def popState = {
@@ -24,13 +26,14 @@ def popState = {
 
 /** Get top of stack */
 def currState = {
-  stateStack[-1]
+  // stateStack[-1]
+  stateStack.first();
 }
 
 def defState = {
   String stateCode,  Closure c ->
-    states[stateCode] = new State(stateCode: stateCode,
-                                  processLine: c);
+  states[stateCode] = new State(stateCode: stateCode,
+                                processLine: c);
 }
 
 
@@ -40,63 +43,65 @@ String lineText;
 
 defState('DEFAULT') {
   String line ->
-    if (line ==~ /\d+/) {
-      lineNum = line;
-      pushState "EXPECT_LINE"
-    } else {
-      println line
-    }
+  if (line =~ /^\d+ *$/) {
+    lineNum = line;
+    System.err.println "line=$lineNum"
+    pushState "EXPECT_LINE"
+    System.err.println "stateStack = $stateStack"
+  } else {
+    println line
+  }
 }
 
 defState('EXPECT_LINE') {
   String line ->
-    def printLine = {
-      println "| ${lineNum} | $lineText | ${lineNum} |"
-      lineText = '';
-    }
-    if (line == lineNum) {
-      printLine()
-      popState()
-      lineNum = null;
-      lineText = '';
-    } else if (line ==~ /[a-z]/) {
-      lineLetter = line;
-      lineText = ''
-      popState()
-      pushState "EXPECT_SUBLINE"
-    } else if (line ==~ /\d+/) {
-      // Starting a new line.
-      popState();
-      currState().processLine(line);
-    }else {
-      lineText += line;
-    }
+  def printLine = {
+    println "| ${lineNum} | $lineText | ${lineNum} |"
+    lineText = '';
+  }
+  if (line == lineNum) {
+    printLine()
+    popState()
+    lineNum = null;
+    lineText = '';
+  } else if (line ==~ /[a-z]/) {
+    lineLetter = line;
+    lineText = ''
+    popState()
+    pushState "EXPECT_SUBLINE"
+  } else if (line ==~ /\d+/) {
+    // Starting a new line.
+    popState();
+    currState().processLine(line);
+  }else {
+    lineText += line;
+  }
 }
 defState('EXPECT_SUBLINE') {
   String line ->
-    String llet = (lineNum?:'') + (lineLetter?.size()?lineLetter:'');
-    def printLine = {
-      println "| ${llet} | $lineText | ${llet} |"
-      lineText = '';
-    }
-    if (line == lineNum) {
-      printLine()
-      popState()
-      lineNum = null;
-      lineText = '';
-    } else if (lineLetter?.size() && line == "$lineNum$lineLetter") {
-      // End of a sub number
-      printLine();
-      lineLetter = null;
-      popState();
-      pushState "EXPECT_LINE"
-    } else if (line ==~ /\d+/) {
-      // Starting a new line.
-      printLine();
-      popState();
-    }else {
-      lineText += line;
-    }
+  String llet = (lineNum?:'') + (lineLetter?.size()?lineLetter:'');
+  def printLine = {
+    println "| ${llet} | $lineText | ${llet} |"
+    lineText = '';
+  }
+  if (line == lineNum) {
+    printLine()
+    popState()
+    lineNum = null;
+    lineText = '';
+  } else if (lineLetter?.size() && line == "$lineNum$lineLetter") {
+    // End of a sub number
+    printLine();
+    lineLetter = null;
+    popState();
+    pushState "EXPECT_LINE"
+  } else if (line ==~ /\d+/) {
+    // Starting a new line.
+    printLine();
+    popState();
+  }else {
+    lineText += line;
+  }
 }
 
 pushState('DEFAULT')
@@ -107,6 +112,6 @@ assert datFile.exists()
 //println "currState() == ${currState()}"
 datFile.readLines().each {
   line ->
-    System.err.println "${currState().stateCode}: $line"
-    currState().processLine(line)
+  System.err.println "${currState().stateCode}: $line"
+  currState().processLine(line)
 }
